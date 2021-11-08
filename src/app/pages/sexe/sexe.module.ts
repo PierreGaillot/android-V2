90df71722b2718a7e9b36732fb237acbbbc2ecd0
GuillaumeGournay <gournay.guillaume@gmail.com>
2021-11-08 11:27:40 +0100

    mergin dev

--- src/app/pages/sexe/sexe.module.ts
+++ src/app/pages/sexe/sexe.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { SexePageRoutingModule } from './sexe-routing.module';
 
 import { SexePage } from './sexe.page';
+import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
 import { QuestionComponent } from 'src/app/components/question/question.component';
 import { SwipeupComponent } from 'src/app/components/swipeup/swipeup.component';
 
@@ -19,6 +18,7 @@ import { SwipeupComponent } from 'src/app/components/swipeup/swipeup.component';
   ],
   declarations: [
     SexePage, 
+    NavbarComponent,
     QuestionComponent,
     SwipeupComponent,
   ]
