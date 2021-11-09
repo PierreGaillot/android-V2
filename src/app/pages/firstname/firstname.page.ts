c48283f0104c063ffcfc1a5ff87e51b39a309489
GuillaumeGournay <gournay.guillaume@gmail.com>
2021-11-09 15:58:54 +0100

    creating userData object

--- src/app/pages/firstname/firstname.page.ts
+++ src/app/pages/firstname/firstname.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import { UserDataModel } from 'src/app/models/user-data.model';
+import { UserDataService } from 'src/app/services/user-data.service';
 
 @Component({
   selector: 'app-firstname',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstnamePage implements OnInit {
 
-  constructor() { }
+  public firstname: string = '';
+  // public userFirstname: UserDataModel;
 
-  ngOnInit() {
+  constructor(
+    public userDataService: UserDataService;
+  ) { }
+
+  ngOnInit() { }
+
+  getFirstame() {
+    this.userDataService.createUser(this.firstname);
   }
 
 }
