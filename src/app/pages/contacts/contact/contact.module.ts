4283d21d50fc666dc8d667eb8d9f747fdbea11dc
pgaillot <p.gaillot@essteam.fr>
2021-11-18 15:44:35 +0100

    add contact page

---
+++ src/app/pages/contacts/contact/contact.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ContactPageRoutingModule } from './contact-routing.module';
+
+import { ContactPage } from './contact.page';
+import { QuestionComponent } from 'src/app/components/question/question.component';
+import { SwipeupComponent } from 'src/app/components/swipeup/swipeup.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ContactPageRoutingModule
+  ],
+  declarations: [
+    ContactPage,
+    QuestionComponent,
+    SwipeupComponent,
+  ]
+})
+export class ContactPageModule {}
