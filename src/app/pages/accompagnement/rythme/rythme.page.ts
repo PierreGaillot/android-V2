006768a516ea559d1eaa1929d512bac6375c7664
GuillaumeGournay <gournay.guillaume@gmail.com>
2021-11-19 16:42:17 +0100

    accompagnement rythme : ok

--- src/app/pages/accompagnement/rythme/rythme.page.ts
+++ src/app/pages/accompagnement/rythme/rythme.page.ts
@@ -8,12 +8,27 @@ import { UserDataService } from 'src/app/services/user-data.service';
 })
 export class RythmePage implements OnInit {
   private contactFrequency: string = '';
+  private disabledStatus: boolean = true;
 
   constructor(
     public userDataService: UserDataService
   ) { }
 
   ngOnInit() {
+  }
+
+  getUserContactFrequency(userContactFrequency) {
+    this.disabledStatus = true;
+    this.userDataService.setUserContactFrequency(userContactFrequency);
+  }
+
+  toggleTextInput() {
+    if (this.disabledStatus == true) return this.disabledStatus = false;
+    return this.disabledStatus = true;
+  }
+
+  getTextContactFrequency(event) {
+    if (event.key === "Enter") return this.userDataService.setUserContactFrequency(this.contactFrequency);
   }
 
 }
