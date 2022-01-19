1abeba1258ca55bd669b10f0f0b9f449a6ee29da
GuillaumeGournay <gournay.guillaume@gmail.com>
2022-01-19 16:00:50 +0100

    RGPD firstname page : OK, routing back to Home : ok

--- src/app/pages/a-propos/modal-rgpd/modal-rgpd.page.ts
+++ src/app/pages/a-propos/modal-rgpd/modal-rgpd.page.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-modal-rgpd',
@@ -14,14 +13,19 @@ export class ModalRGPDPage implements OnInit {
 
   constructor(
     public modalCtrl:ModalController,
-  ) { 
-  }
+    private router:Router
+  ) { }
 
   ngOnInit() {
   }
 
   async closeModal(){
-    await this.modalCtrl.dismiss()
+    await this.modalCtrl.dismiss();
+  }
+  
+  async navigateToHome() {
+    this.router.navigate(['/home']);
+    await this.modalCtrl.dismiss();
   }
 
 }
